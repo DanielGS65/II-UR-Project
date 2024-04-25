11406fe159621f434332ab22df0ed6f52b1135e2
piponaz0 <calvamorejon@gmail.com>
2024-04-25 16:51:52 +0200

    por fin tengo visual studio y no la lío con las ramas

---
+++ URProject/Forms/FormAdriana.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace URProject.Forms
+{
+    partial class FormAdriana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormAdriana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormAdriana";
+            this.Text = "FormAdriana";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
