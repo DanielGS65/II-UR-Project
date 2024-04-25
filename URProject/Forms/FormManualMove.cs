58d9c374cc47f194b0e578d8c50d707d9a1a309d
Dioni <eiji.onizuka@gmail.com>
2024-04-25 20:52:31 +0200

    Menu dioni

--- URProject/Forms/FormManualMove.cs
+++ URProject/Forms/FormManualMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,21 @@ namespace URProject.Forms {
     public partial class FormManualMove : Form {
         public FormManualMove() {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
