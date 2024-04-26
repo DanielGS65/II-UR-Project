7bf3d34707e5fbe33396f3fbd1787a6973a36581
Dioni <eiji.onizuka@gmail.com>
2024-04-26 20:20:03 +0200

    Funciones en botones.

--- URProject/Forms/FormManualMove.cs
+++ URProject/Forms/FormManualMove.cs
@@ -14,6 +14,11 @@ namespace URProject.Forms {
     public partial class FormManualMove : Form {
         ClassRTDE classRTDE;
         ClassControl controlClass;
+
+        // Variables
+        private int speed; // Variable para almacenar la velocidad
+        private int accel; // Variable para almacenar la acceleracion
+        private int precision; // Variable para almacenar la precision
         public FormManualMove(ClassRTDE rtdeClass, ClassControl controlClass) {
             InitializeComponent();
             classRTDE = rtdeClass;
@@ -26,17 +31,7 @@ namespace URProject.Forms {
             this.controlClass = controlClass;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
         {
 
         }
@@ -61,10 +56,6 @@ namespace URProject.Forms {
 
         }
 
-        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void label19_Click(object sender, EventArgs e)
         {
@@ -79,6 +70,77 @@ namespace URProject.Forms {
         private void label16_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void buttonMove_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Name)
+            {
+                // Posicion TCP
+                case "button_x_up":  // X-
+                    ClassData.currentPos[0] -= 1;
+                break;
+                case "button_x_down":  // X+
+                    ClassData.currentPos[0] += 1;
+                break;
+                case "button_y_left":  // Y-
+                    ClassData.currentPos[1] -= 1;
+                break;
+                case "button_y_right":  // Y+
+                    ClassData.currentPos[1] += 1;
+                break;
+                case "button_z_up":  // Z+
+                    ClassData.currentPos[2] += 1;
+                break;
+                case "button_z_down":  // Z-
+                    ClassData.currentPos[2] -= 1;
+                break;
+                // Orientacion TCP
+                case "button_rx_left":  // RX+
+                    ClassData.currentPos[3] += 1;
+                break;
+                case "button_rx_right":  // RX-
+                    ClassData.currentPos[3] -= 1;
+                break;
+                case "button_ry_down":  // RY+
+                    ClassData.currentPos[4] += 1;
+                break;
+                case "button_ry_up":  // RY-
+                    ClassData.currentPos[4] -= 1;
+                break;
+                case "button_rz_left":  // RZ+
+                    ClassData.currentPos[5] += 1;
+                break;
+                case "button_rz_right":  // RZ-
+                    ClassData.currentPos[5] -= 1;
+                break;
+            }
+
+            //if (button.Name.Contains("z_up"))
+            //{
+                //ClassData.currentPos[2] += 1;
+            //}
+            controlClass.moveRobot(ClassData.currentPos);
+        }
+
+        private void numeric_speed_ValueChanged(object sender, EventArgs e)
+        {
+            // Actualiza la variable speed cada vez que el valor cambie
+            speed = (int)numeric_speed.Value;
+        }
+
+        private void numeric_accel_ValueChanged(object sender, EventArgs e)
+        {
+            // Actualiza la variable speed cada vez que el valor cambie
+            accel = (int)numeric_accel.Value;
+        }
+
+        private void numeric_precision_ValueChanged(object sender, EventArgs e)
+        {
+            // Actualiza la variable speed cada vez que el valor cambie
+            precision = (int)numeric_precision.Value;
         }
 
         private void button_up_Click(object sender, EventArgs e) {
