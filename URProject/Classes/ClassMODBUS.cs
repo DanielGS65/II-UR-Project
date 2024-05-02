899cae7bf2a8b0845bcd087160b961571b2020f2
Alex Navarro Soria <alex1yese@gmail.com>
2024-05-02 19:27:01 +0200

    añadido bool para el control del evento

--- URProject/Classes/ClassMODBUS.cs
+++ URProject/Classes/ClassMODBUS.cs
@@ -8,13 +8,22 @@ using EasyModbus;
 
 namespace URProject.Classes
 {
+
     public class ClassMODBUS
     {
-        public void writeRegisterIOLink(int valor)
+
+        public bool IOLinkStatus { 
+            get { return this.IOLinkStatus; } set { writeRegisterIOLink(value); } }
+        public void writeRegisterIOLink(bool valor)
         {
+            int numValor = 0;
+            if (valor)
+            {
+                numValor = 1;
+            } 
             ModbusClient modbusClient = new ModbusClient(ClassData.ipIOlink, ClassData.portIOlink);
             modbusClient.Connect();
-            modbusClient.WriteSingleRegister(ClassData.registerIOLink, valor);
+            modbusClient.WriteSingleRegister(ClassData.registerIOLink, numValor);
         }
 
 
